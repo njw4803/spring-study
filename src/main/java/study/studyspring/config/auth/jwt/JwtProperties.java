ac45c20ee5b37dcfe0f69ef3d5ee865aaf24b327
nojiwon1227 <nojiwon1227@gmail.com>
2024-02-19 23:38:51 +0900

    JwtProperties 수정

--- src/main/java/study/studyspring/config/auth/jwt/JwtProperties.java
+++ src/main/java/study/studyspring/config/auth/jwt/JwtProperties.java
@@ -1,8 +1,10 @@
 package study.studyspring.config.auth.jwt;
 
 public interface JwtProperties {
+
     String SECRET = "cos"; // 우리 서버만 알고 있는 비밀값
     int EXPIRATION_TIME = 60000*10; //10분
     String TOKEN_PREFIX = "Bearer ";
     String HEADER_STRING = "Authorization";
+
 }
