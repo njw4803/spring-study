9340aef166f138ce505a2e0509700c2ff5b75548
nojiwon1227 <nojiwon1227@gmail.com>
2024-02-20 23:12:09 +0900

    AlreadyRegisteredUserException 수정

--- src/main/java/study/studyspring/exception/AlreadyRegisteredUserException.java
+++ src/main/java/study/studyspring/exception/AlreadyRegisteredUserException.java
@@ -4,6 +4,7 @@ package study.studyspring.exception;
  * 이미 등록된 유저를 재등록하려고 할때 발생하는 Exception
  */
 public class AlreadyRegisteredUserException extends RuntimeException {
+
     public AlreadyRegisteredUserException(String message) {
         super(message);
     }
