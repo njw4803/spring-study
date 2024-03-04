c759a2b7bbd5d22407b120a2ec219f78961ab4b5
nojiwon1227 <nojiwon1227@gmail.com>
2024-03-05 01:43:01 +0900

    간단한 웹소켓 js 적용

--- src/main/java/study/studyspring/handler/WebSocketHandler.java
+++ src/main/java/study/studyspring/handler/WebSocketHandler.java
@@ -1,5 +1,6 @@
 package study.studyspring.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -10,17 +11,20 @@ import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
+@Slf4j
 public class WebSocketHandler extends TextWebSocketHandler {
 
-    private static final ConcurrentHashMap<String, WebSocketSession> CLIENTS = new ConcurrentHashMap<String, WebSocketSession>();
+    private static final ConcurrentHashMap<String, WebSocketSession> CLIENTS = new ConcurrentHashMap<>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        System.out.println("WebSocket 연결" + this);
         CLIENTS.put(session.getId(), session);
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        System.out.println("WebSocket 종료" + this);
         CLIENTS.remove(session.getId());
     }
 
